bf674e2b70e4ebe0992da7438cabc7e78fbcfe7b
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-10-09 15:47:05 -0700

    add event listeners to handle user and role updates while the bot is running

---
+++ db/helpers.js
@@ -0,0 +1,224 @@
+const knex = require('./knex');
+
+/**
+ * Handle syncing users from discord to database on startup
+ *
+ * @param guild
+ * @returns {Promise<void>}
+ */
+async function syncUsers (guild) {
+    console.log('Syncing Users...');
+
+    const dbUsers = await knex.select().table('discord_users');
+
+    const usersToDelete = new Set();
+    const dbUsersMap = new Map();
+
+    // initialize our sets with database members
+    dbUsers.forEach((row) => {
+        usersToDelete.add(row.discord_user_id);
+        dbUsersMap.set(row.discord_user_id, row);
+    });
+
+    guild.members.cache.each(async (guildMember) => {
+        // insert members who have joined while the bot was down
+        if (!dbUsersMap.has(guildMember.user.id)) {
+            await addUser(guildMember);
+        } else {
+            // remove the users from the set so they wont be deleted
+            usersToDelete.delete(guildMember.user.id);
+        }
+    });
+
+    if (usersToDelete.size > 0) {
+        // flag user as quit
+        await knex('discord_members')
+            .whereIn('discord_user_id', Array.from(usersToDelete))
+            .update({quit_timestamp: Date.now()});
+
+        // delete user's roles
+        await knex('discord_roles_users').whereIn('discord_user_id', Array.from(usersToDelete)).del();
+    }
+
+    console.log('Syncing Users Complete');
+}
+
+/**
+ * Handle syncing roles from discord to database on startup
+ *
+ * @param guild
+ * @returns {Promise<void>}
+ */
+async function syncRoles (guild) {
+    console.log('Syncing Users...');
+
+    const dbRoles = await knex.select().table('discord_roles');
+
+    const rolesToDelete = new Set();
+    const dbRolesMap = new Map();
+
+    // initialize our sets with database roles
+    dbRoles.forEach((row) => {
+        rolesToDelete.add(row.discord_role_id);
+        dbRolesMap.set(row.discord_role_id, row);
+    });
+
+    guild.roles.cache.each(async (role) => {
+        // insert roles which were created while the bot was down
+        if (!dbRolesMap.has(role.id)) {
+            await addRole(role);
+        } else {
+            // remove the users from the set so they wont be deleted
+            rolesToDelete.delete(role.id);
+        }
+    });
+
+    // delete roles that were deleted while the bot was down
+    if (rolesToDelete.size > 0) {
+        await knex('discord_roles')
+            .delete()
+            .whereIn('discord_role_id', Array.from(rolesToDelete));
+    }
+
+    console.log('Syncing Roles Complete');
+}
+
+/**
+ * Handle syncing all users roles from discord to database on startup
+ *
+ * @param guild
+ * @returns {Promise<void>}
+ */
+async function syncUsersRoles (guild) {
+    console.log('Syncing Users Roles...');
+
+    const dbUsers = await knex.select()
+        .whereNull('quit_timestamp')
+        .table('discord_users');
+
+    for (const row of dbUsers) {
+        const guildMember = guild.members.cache.get(row.discord_user_id);
+
+        await syncUserRoles(guildMember, row);
+    }
+
+    console.log('Syncing Users Roles Complete');
+}
+
+/**
+ * Add a new role to the database
+ *
+ * @param role
+ * @returns {Knex.QueryBuilder<{discord_role_id, discord_role_name, discord_role_position}, number[]>}
+ */
+function addRole (role) {
+    return knex('discord_roles').insert([{
+        discord_role_id: role.id,
+        discord_role_name: role.name,
+        discord_role_position: role.position
+    }]);
+}
+
+/**
+ * Update an existing role in the database
+ *
+ * @param role
+ * @returns {Knex.QueryBuilder<TRecord, number>}
+ */
+function updateRole (role) {
+    return knex('discord_roles')
+        .where({discord_role_id: role.id})
+        .update({
+            discord_role_name: role.name,
+            discord_role_position: role.position
+        });
+}
+
+/**
+ * Delete a role from the database
+ *
+ * @param role
+ * @returns {Knex.QueryBuilder<TRecord, number>}
+ */
+function deleteRole (role) {
+    return knex('discord_roles')
+        .where({discord_role_id: role.id})
+        .del();
+}
+
+/**
+ * Add a new member to the database
+ *
+ * @param guildMember
+ * @returns {Knex.QueryBuilder<{discord_nickname: string, inactivity_excused_until: null, discord_user_id, discord_avatar_hash: null, join_date: Date, quit_timestamp: null, discord_username, is_bot: boolean | APIUser | User, discord_discriminator: string, age: null}, number[]>}
+ */
+function addUser (guildMember) {
+    return knex('discord_users').insert([{
+        discord_user_id: guildMember.user.id,
+        discord_username: guildMember.user.username,
+        discord_discriminator: guildMember.user.discriminator,
+        discord_nickname: guildMember.nickname,
+        discord_avatar_hash: guildMember.user.avatar,
+        age: null,
+        is_bot: guildMember.user.bot,
+        join_date: guildMember.joinedAt,
+        inactivity_excused_until: null,
+        quit_timestamp: null
+    }]);
+}
+
+/**
+ * Update a database use with new fields
+ *
+ * @param guildMember
+ * @param fields
+ * @returns {Knex.QueryBuilder<TRecord, number>}
+ */
+function updateUser (guildMember, fields) {
+    return knex('discord_users')
+        .where({discord_user_id: guildMember.id})
+        .update(fields);
+}
+
+/**
+ * Sync a discord member's roles to the database
+ *
+ * @param guildMember
+ * @param dbUser
+ * @returns {Promise<void>}
+ */
+async function syncUserRoles (guildMember, dbUser = null) {
+    console.log(`Syncing Roles For ${guildMember.displayName}`);
+
+    const roles = guildMember.roles.cache;
+
+    // first clear the user's roles
+    if (dbUser) {
+        await knex('discord_roles_users').where('discord_user_id', dbUser.discord_user_id).del();
+    }
+
+    const trx = await knex.transaction();
+
+    const entries = roles.map((role) => {
+        return {
+            discord_user_id: guildMember.id,
+            discord_role_id: role.id
+        };
+    });
+
+    await trx('discord_roles_users')
+        .insert(entries)
+        .then(() => trx.commit());
+}
+
+module.exports = {
+    syncUsers,
+    syncRoles,
+    syncUsersRoles,
+    addUser,
+    updateUser,
+    syncUserRoles,
+    addRole,
+    updateRole,
+    deleteRole
+}
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 require('./sentry').init();
 
 const {Client} = require('discord.js');
-const {syncUsers, syncRoles, syncUserRoles} = require('./db/sync');
+const {syncUsers, syncRoles, syncUsersRoles, addUser, updateUser, syncUserRoles, addRole, updateRole, deleteRole} = require('./db/helpers');
+const knex = require('./db/knex');
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const GUILD_ID = process.env.GUILD_ID;
@@ -11,9 +12,113 @@ const client = new Client({
     intents : ['GUILDS', 'GUILD_PRESENCES', 'GUILD_MEMBERS']
 });
 
+/**
+ * Handle adding new discord member to database on guild join
+ */
+client.on('guildMemberAdd', async (guildMember) => {
+    const userRows = await knex('discord_users')
+        .where({ discord_user_id:  guildMember.id});
+
+    // user exists in the database means they have been here before
+    if (userRows.length === 1) {
+        // update their user record
+        await updateUser(guildMember, {
+            discord_username: guildMember.user.username,
+            discord_discriminator: guildMember.user.discriminator,
+            discord_nickname: guildMember.nickname,
+            quit_timestamp: null
+        });
+
+        // sync their roles
+        await syncUserRoles(guildMember, userRows[0]);
+    } else if (userRows.length > 1) {
+        throw new Error(`Found user with id ${guildMember.id} and display name ${guildMember.displayName} in database more than once!`);
+    } else {
+        // brand new user, add them
+        await addUser(guildMember);
+
+        // sync their roles
+        await syncUserRoles(guildMember);
+    }
+});
+
+/**
+ * Handle flagging guild member as quit and remove all roles in the database
+ */
+client.on('guildMemberRemove', async (guildMember) => {
+    const userRows = await knex('discord_users')
+        .where({ discord_user_id:  guildMember.id});
+
+    if (userRows.length > 0) {
+        updateUser(guildMember, {
+            quit_timestamp: Date.now()
+        });
+
+        await knex('discord_roles_users').where('discord_user_id', userRows[0].discord_user_id).del();
+    }
+});
+
+/**
+ * Handle adding new roles to the database
+ */
+client.on('roleCreate', async (role) => {
+    await addRole(role);
+});
+
+/**
+ * Handle updating a role in the database
+ */
+client.on('roleUpdate', async (oldRole, newRole) => {
+    await updateRole(newRole);
+});
+
+/**
+ * Handle deleting a role from the database
+ */
+client.on('roleDelete', async (role) => {
+    await deleteRole(role);
+});
+
+/**
+ * Handle nickname role updates for a user
+ */
+client.on('guildMemberUpdate', async (oldGuildMember, newGuildMember) => {
+    if (oldGuildMember.nickname !== newGuildMember.nickname) {
+        // updated nickname
+        updateUser(newGuildMember, {
+            discord_nickname: newGuildMember.nickname,
+        });
+    } else if (oldGuildMember.roles.cache.size !== newGuildMember.roles.cache.size) {
+        await syncUserRoles(newGuildMember);
+    } else {
+        console.warn('Untracked guildMemberUpdate')
+    }
+});
+
+/**
+ * Handle username or user  discriminator changes
+ */
+client.on('userUpdate', async (oldUser, newUser) => {
+    await updateUser(newUser, {
+        discord_username: newUser.username,
+        discord_discriminator: newUser.discriminator,
+        discord_avatar_hash: newUser.avatar
+    });
+});
+
 client.on('ready', async () => {
     // eslint-disable-next-line no-console
     console.info(`Logged in as ${client.user.tag}!`);
+
+    // register event listeners for the following (but not limited to) events
+    // 1. user join - done
+    // 2. user leave - done
+    // 3. nickname change - done
+    // 4. role add - done
+    // 5. role delete - done
+    // 6. role edit - done
+    // 7. user role add - done
+    // 8. user role remove - done
 
     const guild = client.guilds.cache.get(GUILD_ID);
 
@@ -25,14 +130,14 @@ client.on('ready', async () => {
     await guild.members.fetch();
     await guild.roles.fetch();
 
-    // sync roles to db
-    await syncRoles(guild);
-
     // add users to db, deactivating users who left
     await syncUsers(guild);
 
+    // sync roles to db
+    await syncRoles(guild);
+
     // sync user roles
-    await syncUserRoles(guild);
+    await syncUsersRoles(guild);
 
     // sync user-roles to db
 });
